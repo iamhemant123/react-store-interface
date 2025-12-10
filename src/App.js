94b44c5b8ca67c2a21e12e9774609b48233d7727
HEMANT KUMAR SINGH <iamhemant639@gmail.com>
2025-12-10 23:06:15 +0530

    Add files via upload
    react-store-interface

---
+++ src/App.js
@@ -0,0 +1,100 @@
+
+import './App.css';
+import Category from './category/Category';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+function App() {
+
+  let [finalCategory, setFinalCategory] = useState([])
+  let [finalPro, setFinalProduct] = useState([])
+  let [catName, setCatname] = useState('')
+
+  let getCategory = () => {
+    axios.get('https://dummyjson.com/products/categories')
+      .then((res) => res.data)
+      .then((finalRes) => {
+        setFinalCategory(finalRes)
+      })
+  }
+
+
+  let getProducts = () => {
+    axios.get('https://dummyjson.com/products')
+      .then((proRes) => proRes.data)
+      .then((finalRes) => {
+        setFinalProduct(finalRes.products);
+      })
+  }
+
+  useEffect(() => {
+    getCategory();
+    getProducts();
+  }, [])
+
+  useEffect(() => {
+    if (catName !== '') {
+      axios.get(`https://dummyjson.com/products/category/${catName}?limit=15`)
+        .then((proRes) => proRes.data)
+        .then((finalRes) => {
+          setFinalProduct(finalRes.products);
+        })
+    }
+  }, [catName])
+
+
+
+  let Pitems = finalPro.map((products, index) => {
+    return (
+      <ProductItems key={index} pdata={products}/>
+    )
+  })
+
+
+  return (
+    <>
+      <div className='py-[40px]'>
+        <div className='max-w-[1320px] mx-auto'>
+          <h1 className='text-center text-[40px] font-bold mb-[30px]'>Our Products</h1>
+          <div className='grid grid-cols-[30%_auto] gap-[20px]'>
+            <div>
+              <Category finalCategory={finalCategory} setCatname={setCatname} />
+            </div>
+
+            <div>
+              <div className='grid grid-cols-3 gap-5'>
+                {
+                  finalPro.length >= 1 ?
+                    Pitems
+                    :
+                    'Loading....'
+
+                }
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
+
+function ProductItems({ pdata }) {
+
+  return (
+    <>
+      <div className='shadow-lg text-center pb-4'>
+        <img src={pdata.thumbnail} className='w-[100%] h-[220px]' alt="img" />
+        <h4>
+          {pdata.title}
+        </h4>
+        <b>
+          Rs {pdata.price}
+        </b>
+      </div>
+    </>
+
+  )
+}
---
+++ src/category/Category.jsx
@@ -0,0 +1,26 @@
+export default function Category({ finalCategory, setCatname }) {
+
+    let cat = finalCategory.map((v, i) => {
+        return (
+            <li 
+                onClick={() => setCatname(v.slug)} 
+                key={i} 
+                className='bg-[#ccc] p-[7px] cursor-pointer text-[20px] font-[500] mb-2'
+            >
+                {v.name}
+            </li>
+        )
+    })
+
+    return (
+        <div>
+            <h3 className='text-[25px] font-[500] p-[10px]'>
+                Product Category
+            </h3>
+
+            <ul>
+                {cat}
+            </ul>
+        </div>
+    );
+}
